51379df11603734e499eb0d1402e7805105a3d31
Agnes Gruszecka <agnieszka.gruszecka@gmail.com>
2020-06-09 11:49:01 +0200

    very basic unittest for add, substract, multiply and divide

---
+++ Unit testting/calc.py
@@ -0,0 +1,23 @@
+# unit testing with Corey Schafer
+
+
+def add(x, y):
+    """"""
+    return x + y
+
+
+def substract(x, y):
+    """"""
+    return x - y
+
+
+def multiply(x, y):
+    """"""
+    return x * y
+
+
+def divide(x, y):
+    """"""
+    if y == 0:
+        raise ValueError('Can not divide by 0!')
+    return x / y
