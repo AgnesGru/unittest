b3a2153dbb76d09c238762941146cb2a04a5e32b
Agnes Gruszecka <agnieszka.gruszecka@gmail.com>
2020-06-15 12:27:32 +0200

    sub classing, overwriting methods

--- Objective Oriented Programing/OOP.py
+++ Objective Oriented Programing/OOP.py
@@ -17,11 +17,9 @@ class Polygons:
         for i in range(self.sides):
             turtle.forward(self.size)  # 100 pixels
             turtle.right(180 - self.angle)  # angle
-        turtle.done()
+
 
 # print("8" * 100)
-
-
 # def draw_function(sides, name, angle, size, color, line_thickness):  # without class method
 #     turtle.color(color)
 #     turtle.pensize(line_thickness)
@@ -29,8 +27,8 @@ class Polygons:
 #         turtle.forward(size)  # 100 pixels
 #         turtle.right(180 - angle)  # angle
 #     turtle.done()
-
 # print("8" * 100)
+
 
 # square = Polygons(4, "Square")
 # pentagon = Polygons(5, "Pentagon")
@@ -44,3 +42,21 @@ class Polygons:
 # ############## without class method ###############
 # draw_function(4, "Square", 90, 100, "yellow", 10) # without class method
 # ###################################################
+## Sub -classing, method overwriting
+
+class Square(Polygons):
+    def __init__(self, size=100, color='red', line_thickness=4):
+        super().__init__(4, "Square", size, color, line_thickness)
+
+    def draw(self):
+        turtle.begin_fill()
+        super().draw()
+        turtle.end_fill()
+
+
+square = Square(color="#abcd11")
+
+print(square.sides)
+print(square.angle)
+square.draw()
+turtle.done()
